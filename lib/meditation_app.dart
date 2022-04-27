0f8c484a212b8722f3b8c3e985f9a35720fa8513
Ngoc Thinh <phanthinh106@gmail.com>
2022-04-28 00:31:16 +0700

    12h31

--- lib/meditation_app.dart
+++ lib/meditation_app.dart
@@ -19,17 +19,37 @@ class HomePage extends StatelessWidget {
         child:  oriantation == Orientation.portrait ?
         Stack(
           children: const [
+            // Align(
+            //   alignment: Alignment.bottomCenter,
+            //   child: FractionallySizedBox(
+            //     heightFactor: 0.6,
+            //     widthFactor: 1,
+            //     child: FittedBox(
+            //       alignment: Alignment.topCenter,
+            //       fit: BoxFit.cover,
+            //       child: SvgPicture.network(getstartedBackgound,
+            //       ),
+            //     ),
+            //   ),
+            // ),
             GetStartedBody(),
             FractionallySizedBox(
                 widthFactor: 1,
-                heightFactor: 0.3,
+                heightFactor: 0.4,
                 child: GetStartedHeader()
             )
                 ,]
       ) :
       Row(
         children: const [
-          Expanded(child: GetStartedHeader(),),
+          Expanded(
+
+            child: FractionallySizedBox(
+              heightFactor: 0.8,
+              alignment: Alignment.topCenter,
+              child: GetStartedHeader(),
+            ),
+          ),
           Expanded(child: GetStartedBody(),)
 
 
@@ -48,13 +68,15 @@ class GetStartedBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
     return Align(
       alignment: Alignment.bottomCenter,
       child: FractionallySizedBox(
-        heightFactor: 0.6,
+        heightFactor: orientation == Orientation.portrait ?  0.6 : 0.9,
         widthFactor: 1,
         child: FittedBox(
           fit: BoxFit.cover,
+          alignment: Alignment.topCenter,
           child: SvgPicture.network(getstartedBackgound),
         ),
       ),
@@ -68,30 +90,53 @@ class GetStartedHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
         children: [
-          Expanded(child: SvgPicture.network(getstartedLogo,
-          ),),
-          Expanded(
-            child: Column(
-              children: [
-                Text('Hi Afsar, Welcome\n',
-                  style: PrimaryFont.medium(30).copyWith(
-                    color: kColorLightYellow
-                  ),
-                ),
-                Text('to Silent Moon',
-                  style: PrimaryFont.thin(30).copyWith(
-                      color: kColorLightYellow,
-                    fontWeight: FontWeight.w100
-                  ),
-                ),
-                Text('Explore the app, Find some peace of mind\nto prepare for meditation.',
-                  style: PrimaryFont.light(16).copyWith(
-                      color: kColorLightGrey
-                  ),
-                ),
+          Flexible(child: SvgPicture.network(getstartedLogo,
+            alignment: const Alignment(0.0, -0.8),
+          ),
+          flex: 2,
+          fit: FlexFit.tight,),
+          Flexible(
+            child: FittedBox(
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    text: 'Hi Afsar, Welcome\n',
+                    style: PrimaryFont.medium(30).copyWith(
+                        color: kColorLightYellow),
+                    children: [
+                      TextSpan(
+                        text: 'to Silent Moon',
+                        style: PrimaryFont.thin(30).copyWith(
+                          color: kColorLightYellow,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
 
-              ],
+                    ]
+                ),
+              ),
             ),
+            flex: 1,
+            fit: FlexFit.tight,
+          ),
+          Flexible(
+            child: SizedBox.expand(
+              child: FractionallySizedBox(
+                widthFactor: 0.8,
+                child: FittedBox(
+                  alignment: Alignment.center,
+                  child: Text('Explore the app, Find some peace of mind\nto prepare for meditation.',
+                    style: PrimaryFont.light(16).copyWith(
+                      color: kColorLightGrey,
+                      height: 1.2
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            flex: 1,
+            fit: FlexFit.tight,
           )
 
         ],
